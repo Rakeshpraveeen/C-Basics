941e5d8a8edfdb73106f84feb8e3a4fb8f26f602
Rakeshpraveeen <92291202+Rakeshpraveeen@users.noreply.github.com>
2021-10-20 12:15:59 +0530

    Add files via upload

---
+++ addition of two numbers.c
@@ -0,0 +1,18 @@
+// addition of two numbers
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	// initialization
+	age=21;
+	cgpa=9.5;
+	initial='E';
+	// out put
+	printf("age of the student is %d\n",age);
+	printf("cgpa of the student is %f\n",cgpa);
+	printf("student surname starts with %c",initial);
+}
